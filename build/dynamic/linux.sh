6238d41c6080e0b60738966219a5d1dfa7c94984
samuelmasse <samuelmasse4@gmail.com>
2020-10-10 23:26:30 -0400

    linux dynamic

---
+++ build/dynamic/linux.sh
@@ -0,0 +1,6 @@
+# RUN THIS FROM THE ROOT OF THE REPO
+gcc -o nfd.so \
+    src/nfd_gtk.c src/nfd_common.c \
+    -Isrc/include `pkg-config gtk+-3.0 --cflags` \
+    -L/usr/lib64 `pkg-config --libs gtk+-3.0` \
+    -shared -m64 -s -fPIC -O2 -DNDEBUG
